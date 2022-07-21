a50bdab3cfb7f9a46d129f967ceb33f1b9ff886d
Artem Ageev <icediver@MacBook-Pro-Artem.local>
2022-07-22 00:55:23 +0300

    add services

---
+++ app/types/user.interface.ts
@@ -0,0 +1,16 @@
+import { IBase } from '@/types/base.interface';
+import { IVideo } from '@/types/video.interface';
+
+export interface IUser extends IBase {
+	email: string;
+	name: string;
+	isVerified?: boolean;
+	subscribersCount?: number;
+	description: string;
+	avatarPath: string;
+	videos?: IVideo[];
+	subscriptions: ISubscription[];
+}
+export interface ISubscription extends IBase {
+	toChanel: IUser;
+}
