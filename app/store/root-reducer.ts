d54fa48c80698de72cb186fd39694cedcad6bdf7
Artem Ageev <icediver@MacBook-Pro-Artem.local>
2022-07-24 09:47:24 +0300

    add store

---
+++ app/store/root-reducer.ts
@@ -0,0 +1,9 @@
+import { reducer as toastrReducer } from 'react-redux-toastr';
+import { combineReducers } from 'redux';
+
+import { authSlice } from '@/store/auth/auth.slices';
+
+export const rootReducer = combineReducers({
+	auth: authSlice.reducer,
+	toastr: toastrReducer,
+});
